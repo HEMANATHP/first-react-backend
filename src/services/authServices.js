5f7541a9f9e38fb3a99c26d6cf834df535743c97
Hemanath <hempanneer2003@gmail.com>
2026-07-29 16:37:10 +0530

    implemented the hashedpassword flow

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,5 +1,5 @@
 import prisma from "../config/prisma.js"
-
+import bcrypt from "bcrypt"
 export const loginService = async(email,password)=>{
     const user = await prisma.user.findUnique({
     where:{email}
@@ -8,9 +8,9 @@ export const loginService = async(email,password)=>{
     if(!user){
         return null
     }
-
-    if(user.password !== password){
-        return null;
+    const ismatched =  await bcrypt.compare(password,user.password)
+    if(!ismatched){
+        return null
     }
     return user
 }
@@ -20,16 +20,18 @@ export const registerService = async(name,email,password)=>{
         where:{
             email
         }
-    })
+    })  
+    const hashedPassword = await bcrypt.hash(password,10)
+
     if(user){
         return null
     }
-     else{
+    else{
        const newUser =  await prisma.user.create({
             data:{
                   name: name,
                   email: email,
-                  password:password
+                  password:hashedPassword
             }
         })
 
